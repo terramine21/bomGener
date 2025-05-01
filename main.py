ff863adc7261616ca1fcfbdb850026f0cb0064a0
terramine21 <kiselivish@yandex.ru>
2025-05-01 15:01:54 +0300

    azaza

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+#делаем фигню, что взамен перечня в эксель табличке выплёвывает спецификацию
+from fastapi import FastAPI
+from db_dataBase.dataBase import init_db
+from routes import url
 
-print("kek")
-print("azaza")
+from contextlib import asynccontextmanager
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_db()
+    yield
+    print("Shutting down...")
+
+app = FastAPI()
+app.include_router(url.router)

7fa7ee3b7a8ecfd74097fa8cbb9a87b276a5ff88
terramine21 <kiselivish@yandex.ru>
2025-05-01 16:14:05 +0300

    .

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, UploadFile, File, Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
-from . import models, schemas, crud
+from . import models, schemas #, crud
 from db.database import SessionLocal, engine
 from .services import bom_parser, pe3_generator
 import os
@@ -11,33 +11,4 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
-
-@app.post("/upload-bom/")
-async def upload_bom(
-        file: UploadFile = File(...),
-        db: Session = Depends(get_db)
-):
-    # Сохраняем файл временно
-    file_path = f"temp_{file.filename}"
-    with open(file_path, "wb") as buffer:
-        buffer.write(file.file.read())
-
-    # Парсим BOM
-    bom_data = bom_parser.parse_altium_bom(file_path)
-
-    # Генерируем ПЭ3
-    pe3_path = "pe3_output.xlsx"
-    pe3_generator.generate_pe3(bom_data, pe3_path)
-
-    # Удаляем временный файл
-    os.remove(file_path)
-
-    return {"message": "ПЭ3 сгенерирован", "path": pe3_path}
--- routes/url.py
+++ routes/url.py
@@ -1,14 +1,41 @@
 #обработка запросов, не более
 
-from fastapi import Request, HTTPException, APIRouter, Depends
-from  fastapi.responses import RedirectResponse
+from fastapi import FastAPI, UploadFile, File, Depends, APIRouter
+from sqlalchemy.orm import Session
+from services import bom_parser, pe3_generator
+from db.database import SessionLocal
+import os
 
 router = APIRouter()
 
-@router.post("/test1")
-def test1(data: str):
-    return {"message": data}
 
-@router.get("/{tested1}")
-def tested1():
-    return "tested1"
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.post("/upload-bom/")
+async def upload_bom(
+        file: UploadFile = File(...),
+        db: Session = Depends(get_db)
+):
+    # Сохраняем файл временно
+    file_path = f"temp_{file.filename}"
+    with open(file_path, "wb") as buffer:
+        buffer.write(file.file.read())
+    return {"message": file.filename}
+
+    # Парсим BOM
+    # bom_data = bom_parser.parse_altium_bom(file_path)
+
+    # Генерируем ПЭ3
+    # pe3_path = "pe3_output.xlsx"
+    # pe3_generator.generate_pe3(bom_data, pe3_path)
+    #
+    # # Удаляем временный файл
+    # os.remove(file_path)
+
+    # return {"message": "ПЭ3 сгенерирован", "path": pe3_path}
